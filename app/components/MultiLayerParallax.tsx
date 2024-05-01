8c7034071c9f0fcf995b7f6b15950b9ec2e646a0
Francesco Culmone <fculmone5@gmail.com>
2024-05-01 02:31:24 -0400

    added parallax

--- app/components/MultiLayerParallax.tsx
+++ app/components/MultiLayerParallax.tsx
@@ -1,24 +1,38 @@
-import { useScroll, useTransform } from "framer-motion";
+"use client";
+
+import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 
 export function MultiLayeredParallax() {
+  const titleRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: titleRef,
+    offset: ["start start", "end start"],
+  });
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+
   return (
-    <div className="w-full h-screen overflow-hidden relative grid place-items-center">
-      <h1 className="font-bold text-white text-7xl md:text-9xl relative z-10">
+    <div className="w-full h-screen overflow-hidden relative grid text-center">
+      <motion.h1
+        style={{ y: textY }}
+        className="font-bold text-white text-7xl md:text-9xl relative justify-top z-10 mt-20"
+      >
         PARALLAX
-      </h1>
-      <div
+      </motion.h1>
+      <motion.div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: `url(/pixel-waterfall-adobe-gif.gif)`,
+          backgroundImage: `url(/pixel-waterfall-adobe-gif-no-eagle.gif)`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
+          y: backgroundY,
         }}
       />
       <div
         className="absolute inset-0 z-20"
         style={{
-          backgroundImage: `url(/pixel-waterfall-adobe-gif.gif)`,
+          backgroundImage: `url(/pixel-waterfall-adobe-gif-no-sky.gif)`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
         }}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
       <MultiLayeredParallax />
+      <div className="h-24 w-full bg-gradient-to-b from-black to-[#06141D] " />
       <div className="w-full bg-[#06141D]">
         <div className="max-w-lg space-y-4 mx-auto py-24 text-neutral-300">
           <p>
